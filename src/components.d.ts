d7ca906806988422355c90131a6873933971452f
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-03-17 20:54:15 -0600

    Remove unused files

--- src/components.d.ts
+++ src/components.d.ts
@@ -12,8 +12,5 @@ declare module '@vue/runtime-core' {
     README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheCounter: typeof import('./components/TheCounter.vue')['default']
-    TheFooter: typeof import('./components/TheFooter.vue')['default']
-    TheInput: typeof import('./components/TheInput.vue')['default']
   }
 }
