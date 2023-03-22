220cb56b13d2c0b011fb7d078978d0f4b6cc95a4
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-03-21 21:25:12 -0600

    Add 404 page

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,18 +1,9 @@
-<script setup lang="ts">
-const router = useRouter()
-const { t } = useI18n()
-</script>
-
 <template>
-  <main p="x4 y10" text="center teal-700 dark:gray-200">
-    <div text-4xl>
-      <div i-carbon-warning inline-block />
+  <div
+    class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8"
+  >
+    <div class="max-w-max mx-auto">
+      <RouterView />
     </div>
-    <RouterView />
-    <div>
-      <button btn text-sm m="3 t8" @click="router.back()">
-        {{ t('button.back') }}
-      </button>
-    </div>
-  </main>
+  </div>
 </template>
