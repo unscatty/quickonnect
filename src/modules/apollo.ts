ab0daca425fe7eb3303a1f3ce8f510b671b289f4
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-03-26 19:53:07 -0600

    Add Apollo module

---
+++ src/modules/apollo.ts
@@ -0,0 +1,7 @@
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { type UserModule } from '~/types'
+import apolloClient from '../services/apollo-client'
+
+export const install: UserModule = ({ app }) => {
+  app.provide(DefaultApolloClient, apolloClient)
+}
---
+++ src/services/apollo-client.ts
@@ -0,0 +1,6 @@
+import { createApolloClient } from '@nhost/apollo'
+import nhost from '~/services/nhost-client'
+
+const apolloClient = createApolloClient({ nhost })
+
+export default apolloClient
